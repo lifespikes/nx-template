dda7adfb47f5df211c6fb2bc30a49a5a0748dcb3
Felipe Valencia <pipeedev@gmail.com>
2023-12-29 17:02:09 -0500

    chore: first commit of a successfull project

---
+++ packages/menu/src/MenuServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lifespikes\Menu;
+
+use Illuminate\Support\ServiceProvider;
+
+class MenuServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ packages/menu/src/Pene.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lifespikes\Menu;
+
+class Pene
+{
+    public static function hello(): string
+    {
+        return 'Hello';
+    }
+}
