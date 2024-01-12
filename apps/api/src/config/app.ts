d4cf98af60624609bd78896712735741af4320a6
santigp258 <santigp258@gmail.com>
2024-01-11 23:29:05 -0500

    chore: rename

--- apps/api/src/config/app.ts
+++ apps/api/src/config/app.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   port: parseInt(process.env.APP_PORT, 10) || 3000,
-  name: process.env.APP_NAME ?? 'Educal',
+  name: process.env.APP_NAME ?? 'Unnamed Restaurant',
   description: process.env.APP_DESCRIPTION ?? 'Awesome API',
 }));
