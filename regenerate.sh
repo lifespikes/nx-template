e1c0e1a991e8a46dab06a3fc669258c441f263be
santigp258 <santigp258@gmail.com>
2024-01-24 15:39:56 -0500

    feat: improve regenerate.sh

--- regenerate.sh
+++ regenerate.sh
@@ -1,5 +1,5 @@
 yarn install
 yarn api:command generate:open-api-file --path=packages/api-requests/src/open-api/api/open-api.json
-cd packages/api-requests && yarn orval
-yarn api:prisma
+yarn workspace @spikey/api-requests orval
+yarn prisma:generate
 nx format:write
