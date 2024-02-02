948f356124d83c086c26e6b696e1b9a5ffb9cddf
santigp258 <santigp258@gmail.com>
2024-02-02 12:50:55 -0500

    tests: fix tests

--- packages/nest-shared/src/lib/app/auth/auth.controller.spec.ts
+++ packages/nest-shared/src/lib/app/auth/auth.controller.spec.ts
@@ -1,14 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from '@spikey/nest-shared/app/auth/strategies/jwt.strategy';
+import { HashService } from '@spikey/nest-shared/app/auth/hash.service';
+import { ConfigModule } from '@nestjs/config';
+import registerConfig from '@spikey/nest-shared/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { CustomPrismaModule } from 'nestjs-prisma';
+import { ExtendedPrismaService } from '@spikey/nest-shared/prisma/extended-prisma.service';
+import { UsersModule } from '@spikey/nest-shared/app/users/users.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [...registerConfig()]
+        }),
+        PassportModule,
+        JwtModule,
+        EventEmitterModule.forRoot({
+          global: true
+        }),
+        CustomPrismaModule.forRootAsync({
+          name: 'PrismaService',
+          useClass: ExtendedPrismaService,
+          isGlobal: true
+        }),
+        UsersModule
+      ],
+      providers: [AuthService, HashService, JwtStrategy],
+      controllers: [AuthController]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- packages/nest-shared/src/lib/app/auth/auth.service.spec.ts
+++ packages/nest-shared/src/lib/app/auth/auth.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '@spikey/nest-shared/app/users/users.module';
+import { CustomPrismaModule } from 'nestjs-prisma';
+import { ExtendedPrismaService } from '@spikey/nest-shared/prisma/extended-prisma.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { HashService } from '@spikey/nest-shared/app/auth/hash.service';
+import { JwtStrategy } from '@spikey/nest-shared/app/auth/strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
+import registerConfig from '@spikey/nest-shared/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [...registerConfig()]
+        }),
+        PassportModule,
+        JwtModule,
+        EventEmitterModule.forRoot({
+          global: true
+        }),
+        CustomPrismaModule.forRootAsync({
+          name: 'PrismaService',
+          useClass: ExtendedPrismaService,
+          isGlobal: true
+        }),
+        UsersModule
+      ],
+      providers: [AuthService, HashService, JwtStrategy]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
